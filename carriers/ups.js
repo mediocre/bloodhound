74a7aba749a72fe49848a96789b3fb89272ec03d
Shawn Miller <shawn@mediocre.com>
2020-12-01 10:55:19 -0600

    Filter undefined activities

--- carriers/ups.js
+++ carriers/ups.js
@@ -157,6 +157,9 @@ function UPS(options) {
                 activitiesList = getActivities(packageInfo);
             }
 
+            // Filter undefined activities
+            activitiesList = activitiesList.filter(a => a);
+
             // UPS Mail Innovations doesn't import USPS data reliably. Fallback to USPS when UPS doesn't provide enough data.
             if (options.usps && activitiesList.length <= 1 && usps.isTrackingNumberValid(trackingNumber)) {
                 return usps.track(trackingNumber, callback);
