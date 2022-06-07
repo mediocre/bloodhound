31f78a6ff2df814f684984d47937ba30771ca97a
Matt Schuette <mschuette@gmail.com>
2022-06-07 10:54:44 -0700

    DHL UTAPI work

--- carriers/dhl.js
+++ carriers/dhl.js
@@ -10,26 +10,22 @@ const DELIVERED_TRACKING_DESCRIPTIONS = ['DELIVERED'];
 // These tracking descriptions should indicate the shipment was shipped (shows movement beyond a shipping label being created)
 const SHIPPED_TRACKING_DESCRIPTIONS = ['ARRIVAL DESTINATION DHL ECOMMERCE FACILITY', 'DEPARTURE ORIGIN DHL ECOMMERCE FACILITY', 'ARRIVED USPS SORT FACILITY', 'ARRIVAL AT POST OFFICE', 'OUT FOR DELIVERY', 'PROCESSED THROUGH SORT FACILITY'];
 
+// In an IMpb number, an initial '420' followed by ZIP or ZIP+4 is part of the barcode but is not supposed to be printed. If the tracking number comes from a barcode scanner, it will have that info.
+// 109124 is a Mailer ID provided by DHL. See https://postalpro.usps.com/shipping/impb/BarcodePackageIMSpec for full IMpb specs.
+const DHL_IMPB_REGEX = new RegExp(/^(?:420(?:\d{9}|\d{5}))?(93\d{3}109124(?:\d{14}|\d{10})\d)$/);
+
 function DHL(options) {
     const usps = new USPS(options && options.usps);
 
     this.isTrackingNumberValid = function(trackingNumber) {
+        // Remove spaces and uppercase
         trackingNumber = trackingNumber.replace(/\s/g, '').toUpperCase();
 
-        if ([/^93612\d{17}$/, /^92612\d{17}$/, /^94748\d{17}$/, /^93748\d{17}$/, /^92748\d{17}$/].some(regex => regex.test(trackingNumber))) {
+        if (DHL_IMPB_REGEX.test(trackingNumber)) {
+            // Strip off the IMpb routing code and ZIP
+            trackingNumber = trackingNumber.replace(DHL_IMPB_REGEX, '$1');
+
             return checkDigit(trackingNumber, [3, 1], 10);
-        }
-
-        if (/^420\d{27}$/.test(trackingNumber)) {
-            return checkDigit(trackingNumber.match(/^420\d{5}(\d{22})$/)[1], [3, 1], 10);
-        }
-
-        if (/^420\d{31}$/.test(trackingNumber)) {
-            if (checkDigit(trackingNumber.match(/^420\d{9}(\d{22})$/)[1], [3, 1], 10)) {
-                return true;
-            } else if (checkDigit(trackingNumber.match(/^420\d{5}(\d{26})$/)[1], [3, 1], 10)) {
-                return true;
-            }
         }
 
         return false;
@@ -46,17 +42,17 @@ function DHL(options) {
             _options.minDate = new Date(0);
         }
 
-        // This is the API being used from: https://www.dhl.com/global-en/home/tracking/tracking-ecommerce.html
+        // This is the API being used from: https://developer.dhl.com/api-reference/shipment-tracking
         const req = {
             forever: true,
             gzip: true,
             headers: {
-                referer: `https://www.dhl.com/global-en/home/tracking/tracking-ecommerce.html?tracking-id=${trackingNumber}`
+                'DHL-API-Key': options.apiKey
             },
             json: true,
             method: 'GET',
             timeout: 5000,
-            url: `https://www.dhl.com/utapi?trackingNumber=${trackingNumber}`
+            url: `https://api-eu.dhl.com/track/shipments?trackingNumber=${trackingNumber}`
         };
 
         async.retry(function(callback) {
